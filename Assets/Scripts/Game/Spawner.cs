9aa9c9eadfc7ea26b0c29b2c20055ce10e11c84f
gulecfatih <56516242+gulecfatih@users.noreply.github.com>
2021-06-15 01:41:45 +0300

    +spanewer script written

---
+++ Assets/Scripts/Game/Spawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject barrier;
+    public Bird birdScript;
+    [SerializeField] float time;
+    private void Start()
+    {
+        StartCoroutine(SpawnObject(time));
+    }
+    public IEnumerator SpawnObject(float time)
+    {
+        while (!birdScript.isDead)
+        {
+         
+            
+            Instantiate(barrier, new Vector3(3, Random.Range(-1.28f, 2.9f),0), Quaternion.identity);
+            yield return new WaitForSeconds(time);
+        }
+        
+    }
+}
